bf02f61ddecd0c5e9ccbfcbebc44207400d2fb04
Vinicius Silvestre <viniciussilvestrebarros@gmail.com>
2018-05-17 17:59:00 -0300

    Carregando arquivos

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="pr-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/uikit.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+    <title>Senhas</title>
+    
+</head>
+<body>
+    <section>        
+        <div class="header">
+            <h2>Senhas dos sistemas ou e-mails</h2>
+        </div>
+        <div class="corpo">
+            <div class="uk-child-width-expand@s uk-text-center" uk-grid>
+                <div>
+                    <div class="uk-card uk-card-default uk-card-body">
+                        <h3>Webmail Objeto</h3><br />
+                        <p class="text-left" style="font-size: 16px !important">                            
+                            <b>Acesso:</b> <a href="http://webmail.objetocomunicacao.com.br" target="_blank">Webmail Objeto</a><br />
+                            <b>Login:</b>&nbsp  suporte@objetocomunicacao.com.br<br />
+                            <b>Senha:</b>&nbsp  <span> #melhor17suporte20#</span><br /><br />                            
+                        </p>
+                        
+                            <!-- <table class="uk-table uk-table-responsive uk-table-divider">
+                                    <tbody>
+                                        <tr>
+                                            <td>Table Data</td>
+                                            <td>Table Data</td>
+                                            <td>Table Data</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Table Data</td>
+                                            <td>Table Data</td>
+                                            <td>Table Data</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Table Data</td>
+                                            <td>Table Data</td>
+                                            <td>Table Data</td>
+                                        </tr>
+                                    </tbody>
+                                </table> -->
+                    </div>
+                </div>
+                <div>
+                    <div class="uk-card uk-card-default uk-card-body">Item</div>
+                </div>
+                <div>
+                    <div class="uk-card uk-card-default uk-card-body">Item</div>
+                </div>
+            </div>
+        </div> 
+        
+        <!-- <div class=" coluna col-col-md-12">
+            <style>
+              p{
+                position: relative;
+                min-height: 1px;
+                padding-right: 15px;
+                padding-left: 15px;
+                float: left;
+                width: 33.33333333%;
+               }
+               
+            </style> 
+            
+                <p ><a href="http://dev17.103dns.com.br/website/index.php/PaginaDinamica/pagina/goiania" target="_self"><img alt="" src="http://dev17.103dns.com.br/website/public/imagem/gerenciador/teste_mmnvd_dev17/691047-970x600-1jpeg.jpeg" style="width: 400px; height: 247px;" /></a></p>            
+                <p ><a href="http://dev17.103dns.com.br/website/index.php/PaginaDinamica/pagina/goiania" target="_self"><img alt="" src="http://dev17.103dns.com.br/website/public/imagem/gerenciador/teste_mmnvd_dev17/691047-970x600-1jpeg.jpeg" style="width: 400px; height: 247px;" /></a></p>            
+                <p ><a href="http://dev17.103dns.com.br/website/index.php/PaginaDinamica/pagina/goiania" target="_self"><img alt="" src="http://dev17.103dns.com.br/website/public/imagem/gerenciador/teste_mmnvd_dev17/691047-970x600-1jpeg.jpeg" style="width: 400px; height: 247px;" /></a></p>            
+                <p ><a href="http://dev17.103dns.com.br/website/index.php/PaginaDinamica/pagina/goiania" target="_self"><img alt="" src="http://dev17.103dns.com.br/website/public/imagem/gerenciador/teste_mmnvd_dev17/691047-970x600-1jpeg.jpeg" style="width: 400px; height: 247px;" /></a></p>            
+                <p ><a href="http://dev17.103dns.com.br/website/index.php/PaginaDinamica/pagina/goiania" target="_self"><img alt="" src="http://dev17.103dns.com.br/website/public/imagem/gerenciador/teste_mmnvd_dev17/691047-970x600-1jpeg.jpeg" style="width: 400px; height: 247px;" /></a></p>            
+                <p ><a href="http://dev17.103dns.com.br/website/index.php/PaginaDinamica/pagina/goiania" target="_self"><img alt="" src="http://dev17.103dns.com.br/website/public/imagem/gerenciador/teste_mmnvd_dev17/691047-970x600-1jpeg.jpeg" style="width: 400px; height: 247px;" /></a></p>    
+                        
+        </div> -->
+    </section>
+</body>
+</html>
